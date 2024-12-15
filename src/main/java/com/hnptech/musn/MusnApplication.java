d30d4d9850efbc57adc51ab5108a208048b1d26e
jjoonior <jjoonior97@gmail.com>
2024-12-15 17:50:19 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/com/hnptech/musn/MusnApplication.java
@@ -0,0 +1,13 @@
+package com.hnptech.musn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusnApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MusnApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hnptech/musn/MusnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnptech.musn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusnApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
