fdab7046b791333f5992c07e311d5716d9d31444
jjoonior <jjoonior97@gmail.com>
2025-01-02 12:48:28 +0900

    Fix: 소셜 로그인

--- src/main/java/com/hnptech/musn/service/GoogleService.java
+++ src/main/java/com/hnptech/musn/service/GoogleService.java
@@ -1,7 +1,10 @@
 package com.hnptech.musn.service;
 
 import com.hnptech.musn.exception.BadRequestException;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -9,13 +12,12 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Map;
 
 @Service
-public class KakaoService {
+public class GoogleService {
 
-  //todo restTemplate config 만들어서 custom handler 적용
   private final RestTemplate restTemplate = new RestTemplate();
 
   public Map<String, Object> getUserInfo(String accessToken) {
-    String userInfoUrl = "https://kapi.kakao.com/v2/user/me";
+    String userInfoUrl = "https://www.googleapis.com/userinfo/v2/me";
 
     HttpHeaders headers = new HttpHeaders();
     headers.set("Authorization", "Bearer " + accessToken);
@@ -26,9 +28,7 @@ public class KakaoService {
       ResponseEntity<Map> response = restTemplate.exchange(userInfoUrl, HttpMethod.GET, request, Map.class);
       return response.getBody();
     } catch (HttpClientErrorException e) {
-      Map body = e.getResponseBodyAs(Map.class);
-      throw new BadRequestException(body.get("msg").toString());
+      throw new BadRequestException("this access token does not exist");
     }
   }
 }
-
